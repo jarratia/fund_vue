927d1844ca686e93e139ee9a16b69a9a47b32425
jarratia <jarratia34@gmail.com>
2023-06-07 07:40:35 -0400

    dd

---
+++ src/components/Counter.vue
@@ -0,0 +1,78 @@
+<template>
+  <h2>{{ customTitle }}</h2>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter}} </p>
+
+  <div>
+    <button class="btn primary" v-on:click="increase">+1</button>
+    <button class="btn warning" v-on:click="decrease">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    props: {
+        title: String,
+        start: {
+            type: Number,
+            required: true,
+            default: 100,
+            //validator
+            validator(value) {
+                return value > 100
+            }
+        }
+    },
+
+    data() {
+        return {
+            counter: this.start
+        }
+    },
+    methods: {
+        getSquareValue() {
+            return this.counter * this.counter
+        },
+
+        //Increase
+        increase () {
+           this.counter++
+        },
+
+        //Decrease
+        decrease () {
+            this.counter--
+        }
+    },
+    computed: {
+        // Las propieades computadas se guardan en cache
+        squareCounter(){
+            return this.counter * this.counter
+        },
+
+        //custom title
+        customTitle() {
+            return this.title || 'Contador'
+        }
+    }
+}
+</script>
+
+<style>
+    .btn {
+        border-radius: 1rem;
+        margin: 0 5px;
+        width: 100px;
+        padding: 30px;
+        font-size: 1rem;
+    }
+    .primary {
+        background-color: rgb(77, 77, 231);
+        color: rgb(255, 255, 255);
+    }
+
+    .warning {
+        background-color: rgb(230, 53, 53);
+        color: rgb(255, 255, 255);
+    }
+</style>
---
+++ src/components/Indecision.vue
@@ -0,0 +1,65 @@
+<template>
+    <img src="https://via.placeholder.com/256" alt="bg">
+    <div class="bg-dark"></div>
+
+    <div class="indecision-container">
+        <input type="text" placeholder="Realiza una pregunta">
+        <p>Recuerda cerrar la pregunta con signo de interrogación</p>
+
+        <div>
+            <h2>¿Seré millonario</h2>
+            <h1>Si, No, ...Pensando</h1>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped>
+
+    img, .bg-dark {
+        height: 100vh;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0px;
+        width: 100vw;
+    }
+
+    .bg-dark {
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .indecision-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0px;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 {
+        margin-top: 150px;
+    }
+
+</style>
